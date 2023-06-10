a562bc925da22ebdbb06f12c327793030b02dd4e
vitheshshetty00 <vitheshshetty23@gmail.com>
2023-06-10 14:29:34 +0530

    Authentication Done!

--- providers/ModalProvider.tsx
+++ providers/ModalProvider.tsx
@@ -1,24 +1,25 @@
 "use client";
 
+import AuthModal from "@/components/AuthModal";
+
 import { useEffect, useState } from "react";
 
 const ModalProvider = () => {
-    const [isMounted, setIsMounted] = useState(false);
+	const [isMounted, setIsMounted] = useState(false);
 
-    useEffect(() => {
-      setIsMounted(true)
-    }, []);
+	useEffect(() => {
+		setIsMounted(true);
+	}, []);
 
-    if(!isMounted) {
-        return null;
-    }
-    
-    return (
-        <>
-            Modals!
-        
-        </>
-    )
-}
+	if (!isMounted) {
+		return null;
+	}
+
+	return (
+		<>
+			<AuthModal />
+		</>
+	);
+};
 
 export default ModalProvider;
