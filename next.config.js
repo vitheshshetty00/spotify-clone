ce13384c288a7fd6d16bc6b73ea4fdad092ad692
vitheshshetty00 <vitheshshetty23@gmail.com>
2023-06-12 08:51:18 +0530

    loading page content and getting songs and images

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: [
+            "uebrjghmwyggqerclgbf.supabase.co",
+        ]
+    }
+}
 
 module.exports = nextConfig
