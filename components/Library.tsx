6dd123a39a10e6d3d99644f6e34f69db01594749
vitheshshetty00 <vitheshshetty23@gmail.com>
2023-06-13 22:09:35 +0530

    clean up

--- components/Library.tsx
+++ components/Library.tsx
@@ -6,6 +6,7 @@ import { useUser } from "@/hooks/useUser";
 import useUploadModal from "@/hooks/useUploadModal";
 import { Song } from "@/types";
 import MediaItem from "./MediaItem";
+import useOnPlay from "@/hooks/useOnPlay";
 
 interface LibraryProps {
 	songs:Song[]
@@ -15,6 +16,8 @@ const Library:React.FC<LibraryProps> = ({songs}) => {
 	const authmodal = useAuthModal();
 	const { user } = useUser();
 	const uploadModal = useUploadModal();
+
+	const onPlay = useOnPlay(songs)
 
 
 	const onClick = () => {
@@ -40,7 +43,7 @@ const Library:React.FC<LibraryProps> = ({songs}) => {
 			<div className="flex flex-col gap-y-2 mt-4 px-3">
 				{songs.map((song) => (
 					<MediaItem 
-						onClick={() => {}}
+						onClick={(id:string) => onPlay(id)}
 						key={song.id}
 						data={song}
 					/>
